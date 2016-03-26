7055b26bb4083d30c6d36ed86551ab2faac13880
Fahim Masud Choudhury <fahim@gobd.co>
2016-03-26 20:01:02 +0600

    Added setUp(), tearDown(), beforeClass() and afterClass()

--- src/io/github/tonmoy71/test/BankAccountTest.java
+++ src/io/github/tonmoy71/test/BankAccountTest.java
@@ -1,43 +1,64 @@
 package io.github.tonmoy71.test;
 
 import io.github.tonmoy71.src.BankAccount;
-import org.junit.Test;
+import org.junit.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by tonmoy on 26-Mar-16.
  */
 public class BankAccountTest {
 
+    BankAccount account;
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+        System.out.println("beforeClass()");
+    }
+
+    @Before
+    public void setUp() {
+        account = new BankAccount(100);
+        System.out.println("setup()");
+    }
+
+
     @Test
     public void testDeposit() throws Exception {
-        BankAccount account = new BankAccount();
         account.deposit(50);
-        assertEquals(account.getBalance(), 50, 0);
+        assertEquals(account.getBalance(), 150, 0);
     }
 
     @Test
     public void testWithdraw() throws Exception {
-        BankAccount account = new BankAccount(50);
         account.withdraw(30);
-        assertEquals(account.getBalance(), 20, 0);
+        assertEquals(account.getBalance(), 70, 0);
     }
 
     @Test
     public void testCalculateInterest() {
-        BankAccount account = new BankAccount(100);
         account.calculateInterest(100);
         assertEquals(account.getBalance(), 115, 0);
-
     }
 
-    @Test (expected = ArithmeticException.class)
-    public void testCalculateMonthlyBalance()
-    {
-        BankAccount account = new BankAccount();
+    @Test(expected = ArithmeticException.class)
+    public void testCalculateMonthlyBalance() {
         account.calculateMonthlyBalance(100, 0);
-
     }
 
+    @After
+    public void tearDown()
+    {
+        account = null;
+        System.out.println("tearDown()");
+    }
+
+
+    @AfterClass
+    public static void afterClass()
+    {
+        System.out.println("afterClass()");
+    }
 }
